2c24f223755b8bacdcc6040e00a77852424030ce
vince zipparro <vincezipparro@vinces-MacBook-Pro.local>
2017-11-16 14:01:41 -0600

    Doc image can't be moved around on screen, returns to origin if not dropped on trash can, disappears when dropped on trash.

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    var fileViewOrigin: CGPoint!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //adding pan gesture to file view
+        addPanGesture(view: fileImageView)
+        fileViewOrigin = fileImageView.frame.origin
+        view.bringSubview(toFront: fileImageView)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addPanGesture(view: UIView)
+    {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
     }
 
+    @objc func  handlePan(sender: UIPanGestureRecognizer)
+    {
+        
+        let fileView = sender.view!
+        
+        switch sender.state {
+        case .began, .changed:
+            moveViewWithPan(view: fileView, sender: sender)
+        case .ended:
+            if fileView.frame.intersects(trashImageView.frame)
+            {
+                deleteView(view: fileView)
+            }
+            else
+            {
+                returnViewToOrigin(view: fileView)
+            }
+        default:
+            break
+        }
+        
+        
+    }
+    
+    //helper functions to refractor switch statement
+    func moveViewWithPan(view: UIView, sender: UIPanGestureRecognizer)
+    {
+        let translation = sender.translation(in: view)
+
+        view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+
+    func returnViewToOrigin(view:UIView)
+    {
+        UIView.animate(withDuration: 0.3, animations: {
+            view.frame.origin = self.fileViewOrigin
+        })
+    }
+    
+    func deleteView(view: UIView)
+    {
+        UIView.animate(withDuration: 0.3, animations: {
+            //need a self. when dealing with closures.
+            view.alpha = 0.0
+        })
+    }
 
 }
 
